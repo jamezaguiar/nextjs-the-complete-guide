64c20cb8d21f8d895585fda70ffb08d699f9fd50
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2024-02-24 12:04:21 -0300

    using more custom components

---
+++ 05-onwards-foodies-starting-project/components/main-header/main-header.js
@@ -0,0 +1,30 @@
+import logoImg from '@/assets/logo.png';
+import Image from 'next/image';
+import Link from 'next/link';
+import { MainHeaderBackground } from './main-header-background';
+import styles from './main-header.module.css';
+
+export function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={styles.header}>
+        <Link href="/" className={styles.logo}>
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}

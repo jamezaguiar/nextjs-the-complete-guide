8e988f837a0bbb246f77ff95f8e52639fcb86b04
jamezaguiar <jamersonalsilva14@gmail.com>
2024-07-25 11:02:39 -0300

    throwing not found errors for individual meals

--- 05-onwards-foodies-starting-project/app/meals/[slug]/page.js
+++ 05-onwards-foodies-starting-project/app/meals/[slug]/page.js
@@ -1,12 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 export default function MealsDetailPage({ params }) {
   const { slug } = params;
 
-  const { title, summary, creator_email, instructions, image, creator } =
-    getMeal(slug);
+  const meal = getMeal(slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+  const { title, summary, creator_email, instructions, image, creator } = meal;
 
   return (
     <>
@@ -26,7 +33,7 @@ export default function MealsDetailPage({ params }) {
         <p
           className={styles.instructions}
           dangerouslySetInnerHTML={{
-            __html: instructions.replace(/\n/g, "<br />"),
+            __html: instructions,
           }}
         ></p>
       </main>

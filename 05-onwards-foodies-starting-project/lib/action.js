e0cce94fc31d1499552a6f5d526b37c477cbb25b
jamezaguiar <jamersonalsilva14@gmail.com>
2024-07-25 15:26:10 -0300

    cache revalidation

--- 05-onwards-foodies-starting-project/lib/action.js
+++ 05-onwards-foodies-starting-project/lib/action.js
@@ -2,6 +2,7 @@
 
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
+import { revalidatePath } from "next/cache";
 
 function isInvalidText(text) {
   return !text || text === null || text === undefined || text.trim() === "";
@@ -40,5 +41,6 @@ export async function handleShareMeal(prevState, formData) {
   }
 
   await saveMeal(meal);
+  revalidatePath("/meals");
   redirect("/meals");
 }

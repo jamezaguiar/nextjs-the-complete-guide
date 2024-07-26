43b83eb6fd1ac3136b3d4f37bc703635a554f2f6
jamezaguiar <jamersonalsilva14@gmail.com>
2024-07-26 11:46:22 -0300

    handling not found errors

--- routing-rendering/app/news/[slug]/page.js
+++ routing-rendering/app/news/[slug]/page.js
@@ -1,8 +1,13 @@
 import { DUMMY_NEWS } from "@/dummy-news";
+import { notFound } from "next/navigation";
 
 export default function NewsDetailPage({ params }) {
   const { slug } = params;
   const news = DUMMY_NEWS.find((news) => news.slug === slug);
+
+  if (!news) {
+    notFound();
+  }
 
   return (
     <article className="news-article">

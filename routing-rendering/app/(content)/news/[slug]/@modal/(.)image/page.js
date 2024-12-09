45e1b9fc4d376950841d052e4daf6ed45f16abe2
jamezaguiar <jamersonalsilva14@gmail.com>
2024-12-09 18:46:12 -0300

    route groups

--- routing-rendering/app/(content)/news/[slug]/@modal/(.)image/page.js
+++ routing-rendering/app/(content)/news/[slug]/@modal/(.)image/page.js
@@ -1,6 +1,11 @@
+"use client";
+
 import { DUMMY_NEWS } from "@/dummy-news";
+import { useRouter } from "next/navigation";
 
 export default function InterceptedImagePage({ params }) {
+  const router = useRouter();
+
   const { slug } = params;
   const news = DUMMY_NEWS.find((news) => news.slug === slug);
 
@@ -10,7 +15,7 @@ export default function InterceptedImagePage({ params }) {
 
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back} />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${news.image}`} alt={news.title} />
